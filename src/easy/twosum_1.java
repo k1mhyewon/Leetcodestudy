f90762f98ac29b02af4e674f5628ae1baebbd61d
k1mhyewon <kimhy0434@gmail.com>
2023-03-16 22:17:23 +0900

    테스트 커밋

---
+++ src/easy/twosum_1.java
@@ -0,0 +1,18 @@
+package easy;
+
+public class twosum_1 {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        // int[] result = twoSum(nums, target);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for(int n:nums){
+            System.out.println(n);
+        }
+
+        return nums;
+    }
+}
