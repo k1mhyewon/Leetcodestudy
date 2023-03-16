039a2370f6c9f7c9111557ca37f09ea60f51fb07
k1mhyewon <kimhy0434@gmail.com>
2023-03-16 22:36:00 +0900

    1 two sum solved

--- src/easy/twosum_1.java
+++ src/easy/twosum_1.java
@@ -5,14 +5,27 @@ public class twosum_1 {
         int[] nums = {2,7,11,15};
         int target = 9;
 
-        // int[] result = twoSum(nums, target);
-    }
+        int[] result_arr = twoSum(nums, target);
 
-    public int[] twoSum(int[] nums, int target) {
-        for(int n:nums){
+        for (int n: result_arr) {
             System.out.println(n);
         }
 
-        return nums;
+
+
     }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
 }
