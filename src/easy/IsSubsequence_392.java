f8f0063e70186cae2333218928fcd9fe59583e52
k1mhyewon <kimhy0434@gmail.com>
2023-03-29 17:34:55 +0900

    392. Is Subsequence

---
+++ src/easy/IsSubsequence_392.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class IsSubsequence_392 {
+
+    public static void main(String[] args) {
+        String s = "abc", t = "ahbgdc";
+
+        System.out.println(isSubsequence(s,t));
+
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+
+
+        return false;
+    }
+}
