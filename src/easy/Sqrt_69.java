0e4c351c23eb6817f546be20ace18664639b6644
k1mhyewon <kimhy0434@gmail.com>
2023-03-31 15:33:02 +0900

    69. Sqrt 수정

--- src/easy/Sqrt_69.java
+++ src/easy/Sqrt_69.java
@@ -8,7 +8,7 @@ public class Sqrt_69 {
     }
 
     public static int mySqrt(int x) {
-        /*int start = 1;
+        int start = 1;
         int end = x;
         int res = 0;
 
@@ -20,26 +20,6 @@ public class Sqrt_69 {
             }
             else {
                 end = mid - 1;
-            }
-        }
-
-        return res;*/
-
-        int start = 0;
-        int end = x;
-        int res = 0;
-
-        while(start <= end){
-            int mid = start + (end-start) / 2;
-            if(mid*mid > x) {
-                end = mid - 1;
-            }
-            else if(mid*mid < x) {
-                start = mid + 1;
-                res = mid;
-            }
-            else {
-                return mid;
             }
         }
 
