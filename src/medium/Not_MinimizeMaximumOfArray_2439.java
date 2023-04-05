6999fd7bf74a65c57ecdd263a1e29b26aaeaefba
k1mhyewon <kimhy0434@gmail.com>
2023-04-05 23:25:45 +0900

    Not solved yet 2439. Minimize Maximum of Array

---
+++ src/medium/Not_MinimizeMaximumOfArray_2439.java
@@ -0,0 +1,19 @@
+package medium;
+
+public class Not_MinimizeMaximumOfArray_2439 {
+    public static void main(String[] args) {
+        int[] nums = {3,7,1,6};
+
+        System.out.println(minimizeArrayValue(nums));
+    }
+
+    public static int minimizeArrayValue(int[] nums) {
+        int min = 0;
+
+        for(int i=0; i< nums.length; i++) {
+
+        }
+
+        return 0;
+    }
+}
